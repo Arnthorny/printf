a4e47515e57efdb2e1c7c60d2ac5f43311776948
CipherPhantom <oyepitan10@gmail.com>
2023-03-27 09:22:59 -0700

    Error-Fix_6

--- printf.c
+++ printf.c
@@ -12,7 +12,7 @@ int _printf(const char *format, ...)
 	var_str *sp_var;
 	char c[2] = {0};
 
-	if (!format)
+	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
 	va_start(args, format);
 
